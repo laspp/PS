10d1bb43058b9f161a9bb91617e488cdffaa0354
davors <davor.sluga@gmail.com>
2026-01-07 15:52:11 +0100

    popravljen kernel process vaja 10

--- vaje/10-CUDA/koda/imageCp/kernel.cu
+++ vaje/10-CUDA/koda/imageCp/kernel.cu
@@ -4,14 +4,12 @@ extern "C" {
 
 // Copy image from input to output
 __global__ void process(unsigned char *img_in, unsigned char *img_out, int width, int height) {
-    // row
-    int j = threadIdx.x + blockIdx.x * blockDim.x;
-    // col
-    int i = threadIdx.y + blockIdx.y * blockDim.y;
-    int ipx = i * width + j;
-    while (ipx < width * height) {
-        img_out[ipx] = img_in[ipx];
-        ipx += blockDim.x * gridDim.x * blockDim.y * gridDim.y;
+
+    for (int i = threadIdx.y + blockIdx.y * blockDim.y; i < height; i += blockDim.y * gridDim.y) {
+        for (int j = threadIdx.x + blockIdx.x * blockDim.x; j < width; j += blockDim.x * gridDim.x) {
+            int ipx = i * width + j;
+            img_out[ipx] = img_in[ipx];
+        }
     }
 }
 #ifdef __cplusplus
